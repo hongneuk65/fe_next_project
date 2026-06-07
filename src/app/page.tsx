505f1221867af4c5c6613e6821d71acab4cdc838
hongneuk65 <vanhonga3@gmail.com>
2026-06-07 18:27:18 +0700

    setup next auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import HomePage from "@/components/layout/homepage";
+import { Button } from "antd";
+import { signIn } from "@/auth"
 
 export default function Home() {
   return (
--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -2,11 +2,16 @@
 import { Button, Col, Divider, Form, Input, Row } from 'antd';
 import { ArrowLeftOutlined } from '@ant-design/icons';
 import Link from 'next/link';
+import { authenticate } from '@/utils/actions';
 
 const Login = () => {
 
     const onFinish = async (values: any) => {
-
+        const { email, password } = values;
+        const res = await authenticate(email,password);
+        console.log("check res:", res)
+        // const data = await signIn("credentials", {email, password, redirect:false})
+        // console.log("check data", data)
     };
 
     return (
